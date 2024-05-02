2e625b3d73c8b6f8bbdf711c72ca863220ac151e
spark <sparking02@yahoo.com.mx>
2024-05-01 18:27:16 -0600

    Telling the story

--- lost_fortune/lost_fortune.cpp
+++ lost_fortune/lost_fortune.cpp
@@ -26,5 +26,22 @@ int main()
 	cout << "Enter your las name: ";
 	cin >> leader;
 
+	// Tell the story
+	cout << "\nA brave group of " << adventurers << " set out on a quest ";
+	cout << "-- in search of the lost treasure of the Ancient Dwarves.";
+	cout << "The group was led by that legendary rogue, " << leader << ".\n";
+	cout << "\nAlong the way, a band of marauding ogres ambushed the party. ";
+	cout << "All fought bravely under the command of " << leader;
+	cout << ", and the ogres were defeated, but at a cost. ";
+	cout << "Of the adventurers, " << killed << " were vanquished, ";
+	cout << "leaving just " << survivors << " in the group.\n";
 
+	cout << "\nThe party was about to give up all hope. ";
+	cout << "But while laying the deceased to rest, ";
+	cout << "they stumbled upon the buried fortune. ";
+	cout << "So the adventurers split " << GOLD_PIECES << " gold pieces.";
+	cout << leader << " held on to the extra " << (GOLD_PIECES % survivors);
+	cout << " pieces to keep things fair of course.\n";
+
+	return 0;
 }
