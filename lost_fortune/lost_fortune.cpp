89d8ab5ac05434667bd06d8e76b8753b7e942ef3
spark <sparking02@yahoo.com.mx>
2024-05-01 18:20:04 -0600

    Getting information from the player

--- lost_fortune/lost_fortune.cpp
+++ lost_fortune/lost_fortune.cpp
@@ -8,3 +8,23 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+
+int main()
+{
+	const int GOLD_PIECES = 900;
+	int adventurers, killed, survivors;
+	string leader;
+
+	// get the information
+	cout << "Welcome to Lost Fortune\n\n";
+	cout << "Please enter the following for your personalized adventure\n";
+	cout << "Enter a number: ";
+	cin >> adventurers;
+	cout << "Enter a number, smaller than the first: ";
+	cin >> killed;
+	survivors = adventurers - killed;
+	cout << "Enter your las name: ";
+	cin >> leader;
+
+
+}
