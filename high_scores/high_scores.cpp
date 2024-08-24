2621129ed4b3688846d2c134de3bffe4df390a34
Sparking02 <victor.cortes_marquez@proton.me>
2024-08-23 20:06:22 -0600

    Demonstrates algorithms

--- high_scores/high_scores.cpp
+++ high_scores/high_scores.cpp
@@ -1,20 +1,54 @@
-// high_scores.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// High Scores
+// Demonstrates algorithms
 
-#include <iostream>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<ctime>
+#include<cstdlib>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<int>::const_iterator iter;
+	cout << "Creating a list of scores.";
+	vector<int> scores;
+	scores.push_back(1500);
+	scores.push_back(3500);
+	scores.push_back(7500);
+	cout << "\nHigh Scores:\n";
+	for (iter = scores.begin(); iter != scores.end(); ++iter)
+	{
+		cout << *iter << endl;
+	}
+	cout << "\nFinding a score.";
+	int score;
+	cout << "\nEnter a score to find: ";
+	cin >> score;
+	iter = find(scores.begin(), scores.end(), score);
+	if (iter != scores.end())
+	{
+		cout << "Score found.\n";
+	}
+	else
+	{
+		cout << "Score not found.\n";
+	}
+	cout << "\nRandomizing scores.";
+	srand(static_cast<unsigned int>(time(nullptr)));
+	random_shuffle(scores.begin(), scores.end());
+	cout << "\nHigh Scores:\n";
+	for (iter = scores.begin(); iter != scores.end(); ++iter)
+	{
+		cout << *iter << endl;
+	}
+	cout << "\nSorting scores.";
+	sort(scores.begin(), scores.end());
+	cout << "\nHigh Scores:\n";
+	for (iter = scores.begin(); iter != scores.end(); ++iter)
+	{
+		cout << *iter << endl;
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
